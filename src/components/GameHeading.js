9cf598ee801de3c13371c731319837d2f0664b9c
himanshu0072 <himanshu2003prajapati@gmail.com>
2024-02-26 15:18:53 +0530

    color added in the balls

--- src/components/GameHeading.js
+++ src/components/GameHeading.js
@@ -3,10 +3,10 @@ import React from "react";
 function GameHeading() {
   return <>
   <div className="gameHeading">
-    <div className="div">Score: 10</div>
-    <div className="div">Poped: 9</div>
-    <div className="div">Wrong Hit: 2</div>
     <div className="div">Timer : 01:00</div>
+    <div className="div">Poped: 0</div>
+    <div className="div">Wrong Hit: 0</div>
+    <div className="div">Score: 0</div>
   </div>
   </>;
 }
--- src/components/Gamebody.js
+++ src/components/Gamebody.js
@@ -1,7 +1,50 @@
 import React from "react";
 
 function Gamebody() {
-  return <div>Gamebody</div>;
+  return (
+    <>
+      <div className="gameBody">
+        <div className="buttons">
+          <button className="ballshape">1</button>
+          <button className="ballshape">5</button>
+          <button className="ballshape">3</button>
+          <button className="ballshape">4</button>
+          <button className="ballshape">0</button>
+          <button className="ballshape">9</button>
+          <button className="ballshape">7</button>
+          <button className="ballshape">7</button>
+          <button className="ballshape">1</button>
+          <button className="ballshape">5</button>
+          <button className="ballshape">3</button>
+          <button className="ballshape">4</button>
+          <button className="ballshape">0</button>
+          <button className="ballshape">0</button>
+          <button className="ballshape">1</button>
+          <button className="ballshape">5</button>
+          <button className="ballshape">3</button>
+          <button className="ballshape">4</button>
+          <button className="ballshape">0</button>
+          <button className="ballshape">9</button>
+          <button className="ballshape">7</button>
+          <button className="ballshape">7</button>
+          <button className="ballshape">1</button>
+          <button className="ballshape">5</button>
+          <button className="ballshape">3</button>
+          <button className="ballshape">4</button>
+          <button className="ballshape">0</button>
+          <button className="ballshape">0</button>
+          <button className="ballshape">3</button>
+          <button className="ballshape">4</button>
+          <button className="ballshape">0</button>
+          <button className="ballshape">9</button>
+          <button className="ballshape">7</button>
+          <button className="ballshape">7</button>
+          <button className="ballshape">1</button>
+          <button className="ballshape">5</button>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Gamebody;
