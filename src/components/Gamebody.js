32b02c8d76ec3b2fc13468242a5deb9b09a67aa8
himanshu0072 <himanshu2003prajapati@gmail.com>
2024-02-26 15:18:53 +0530

    design are breing ready..:)

--- src/components/Gamebody.js
+++ src/components/Gamebody.js
@@ -1,7 +1,44 @@
 import React from "react";
 
 function Gamebody() {
-  return <div>Gamebody</div>;
+  return (
+    <>
+      <div className="gameBody">
+        <div className="buttons">
+          <button>1</button>
+          <button>5</button>
+          <button>3</button>
+          <button>4</button>
+          <button>0</button>
+          <button>9</button>
+          <button>7</button>
+          <button>7</button>
+          <button>1</button>
+          <button>5</button>
+          <button>3</button>
+          <button>4</button>
+          <button>0</button>
+          <button>5</button>
+          <button>3</button>
+          <button>4</button>
+          <button>0</button>
+          <button>4</button>
+          <button>0</button>
+          <button>9</button>
+          <button>7</button>
+          <button>7</button>
+          <button>1</button>
+          <button>5</button>
+          <button>3</button>
+          <button>4</button>
+          <button>0</button>
+          <button>9</button>
+          <button>7</button>
+          <button>8</button>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Gamebody;
